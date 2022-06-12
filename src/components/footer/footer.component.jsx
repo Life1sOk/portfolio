6dadf0ee79214f96d123c0b3ec195ac4f59bcd20
Life1sOk <mr.lifeisok@gmail.com>
2022-06-12 21:37:28 +0600

    contacts footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-
+import { FaPhoneAlt } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
 
 import './footer.style.scss';
-import Form from '../form/form.component';
 import ContactIcons from '../contact-icons/contact.icons.component';
 
 const Footer = () => {
  
     return(
         <div className="footer-container">
-            {/* <Form /> */}
+            <div className='send-me-location'>
+                <p>Base in Kazakhstan, Almaty</p>
+                <div className='contact-line'> 
+                    <AiOutlineMail className='contact-line-icon'/>
+                    <p>mr.lifeisok@gmail.com</p>
+                </div>
+                <div className='contact-line'>
+                    <FaPhoneAlt className='contact-line-icon'/>
+                    <p>(xxx) xxx xx xx</p>
+                </div>
+            </div>
             <ContactIcons />
         </div>
     );
