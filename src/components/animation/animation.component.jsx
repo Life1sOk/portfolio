c064c7ae09ab821ef61c82db5741fb2f577cc626
Life1sOk <mr.lifeisok@gmail.com>
2022-07-01 20:49:22 +0600

    ehhhh, animation...

--- src/components/animation/animation.component.jsx
+++ src/components/animation/animation.component.jsx
@@ -5,7 +5,14 @@ import './animation.style.scss';
 const Animation = () => {
     return (
         <div className="animation-container">
-
+            <div className="animation-1">1</div>
+            <div className="animation-2">2</div>
+            <div className="animation-3">3</div>
+            <div className="animation-4">4</div>
+            <div className="animation-5">5</div>
+            <div className="animation-6">6</div>
+            <div className="animation-7">7</div>
+            <div className="animation-8">8</div>
         </div>
     )
 }
