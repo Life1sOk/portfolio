e3481f50b4af46c6269259950b9c1b934b33e87a
Life1sOk <mr.lifeisok@gmail.com>
2023-02-11 13:03:47 +0600

    qwe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import './App.css';
 import Navbar from "./routers/navbar/navbar.component";
