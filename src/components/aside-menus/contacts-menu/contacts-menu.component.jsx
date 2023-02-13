7b293f1539692b9eeaf8fd1849c0e897bdcd4e31
Life1sOk <mr.lifeisok@gmail.com>
2023-02-13 21:09:43 +0600

    qwe

--- src/components/aside-menus/contacts-menu/contacts-menu.component.jsx
+++ src/components/aside-menus/contacts-menu/contacts-menu.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {} from './contacts-menu.style.component';
+import { SizeWrapper } from './contacts-menu.style.component';
 
 import Contacts from "../../contacts/contacts.component";
 import AsideWrapper from "../../aside-wrapper/aside-wrapper.component";
@@ -8,7 +8,9 @@ import AsideWrapper from "../../aside-wrapper/aside-wrapper.component";
 const ContactsMenu = ({ open, openToggle }) => {
     return(
         <AsideWrapper open={open} openToggle={openToggle}>
-            <Contacts gap='50px'/>
+            <SizeWrapper>
+                <Contacts gap='50px'/>
+            </SizeWrapper>
         </AsideWrapper>
     )
 };
--- src/components/aside-menus/contacts-menu/contacts-menu.style.component.js
+++ src/components/aside-menus/contacts-menu/contacts-menu.style.component.js
@@ -1,2 +1,5 @@
 import styled from "styled-components";
 
+export const SizeWrapper = styled.div`
+    transform: scale(1.2);
+`;
