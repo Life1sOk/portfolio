a9c962392337f050fca20f0268a5904b681b0249
Life1sOk <mr.lifeisok@gmail.com>
2023-02-14 17:15:24 +0600

    qwe

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { HashRouter } from 'react-router-dom';
-
-// Fonts : 1) Kalam
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,24 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
-// min-width should 320px;
-// X-Small devices (portrait phones, less than 576px)
-// @media (max-width: 575.98px) { ... }
-
-// Small devices (landscape phones, less than 768px)
-// @media (max-width: 767.98px) { ... }
-
-// Medium devices (tablets, less than 992px)
-// @media (max-width: 991.98px) { ... }
-
-// Large devices (desktops, less than 1200px)
-// @media (max-width: 1199.98px) { ... }
-
-// X-Large devices (large desktops, less than 1400px)
-// @media (max-width: 1399.98px) { ... }
-
-// For Mobile Devices – 320px — 480px
-// iPads and Tablets – 481px — 768px
-// Laptops and small screen – 769px — 1024px
-// Large screens and Desktops – 1025px — 1200px
-// TV and Extra Large Screens – 1201px and more
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
