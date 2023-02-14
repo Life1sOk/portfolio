f8f50ad14304cd98664ec1b10058e4ef1d22bbc9
Life1sOk <mr.lifeisok@gmail.com>
2023-02-14 13:07:20 +0600

    Updates

---
+++ precache-manifest.759060e83f5ef5d52d12dde7f5972af8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f707b9cefa3d3ee7ba",
+    "url": "/portfolio/static/css/main.3568ca82.chunk.css"
+  },
+  {
+    "revision": "e6f707b9cefa3d3ee7ba",
+    "url": "/portfolio/static/js/main.e6f707b9.chunk.js"
+  },
+  {
+    "revision": "834e684d862119c41dac",
+    "url": "/portfolio/static/js/1.834e684d.chunk.js"
+  },
+  {
+    "revision": "37fd682f4fe72dfbc793",
+    "url": "/portfolio/static/js/2.37fd682f.chunk.js"
+  },
+  {
+    "revision": "53ba393285306ba7a083",
+    "url": "/portfolio/static/js/3.53ba3932.chunk.js"
+  },
+  {
+    "revision": "d8ba1728fd3479edc960",
+    "url": "/portfolio/static/js/4.d8ba1728.chunk.js"
+  },
+  {
+    "revision": "049a330bbd161fd290ee",
+    "url": "/portfolio/static/js/5.049a330b.chunk.js"
+  },
+  {
+    "revision": "1073b136d1d6b87b325a",
+    "url": "/portfolio/static/js/runtime~main.1073b136.js"
+  },
+  {
+    "revision": "25cd75a788c75d013207b78a00c9f6e4",
+    "url": "/portfolio/static/media/andrey-high-resolution-logo-color-on-transparent-background.25cd75a7.png"
+  },
+  {
+    "revision": "2f55debd98b4040009b3ab545969577d",
+    "url": "/portfolio/static/media/github.2f55debd.svg"
+  },
+  {
+    "revision": "6aa5ae14c1d36144fb20349160df960d",
+    "url": "/portfolio/static/media/facebook.6aa5ae14.svg"
+  },
+  {
+    "revision": "2e59f048a1b069278d290138c504e63a",
+    "url": "/portfolio/static/media/twitter.2e59f048.svg"
+  },
+  {
+    "revision": "640dfc1cb502a0b0e504d75a4ecd7c16",
+    "url": "/portfolio/static/media/linkedin.640dfc1c.svg"
+  },
+  {
+    "revision": "331e64089dc23537ba26ae3a17b7fbc0",
+    "url": "/portfolio/static/media/Andrey_Tsoy_ru.331e6408.pdf"
+  },
+  {
+    "revision": "fc05b3c6fc9849db7f4671d5f007b19b",
+    "url": "/portfolio/static/media/Andrey_Tsoy_eng.fc05b3c6.pdf"
+  },
+  {
+    "revision": "5c6e1aee064f30c72d17332c2de65135",
+    "url": "/portfolio/static/media/Andrey_Tsoy_cz.5c6e1aee.pdf"
+  },
+  {
+    "revision": "3d6b968bd5b7856010fec4cb8a721aa0",
+    "url": "/portfolio/static/media/IMG_3531.3d6b968b.jpg"
+  },
+  {
+    "revision": "6d7546b98c4fedb54083199eee0361bb",
+    "url": "/portfolio/index.html"
+  }
+];
