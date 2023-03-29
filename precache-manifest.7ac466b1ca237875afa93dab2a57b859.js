1bd8a1186c2e8790c17de9c277b404e5b1723e11
Life1sOk <mr.lifeisok@gmail.com>
2023-03-29 19:34:52 +0600

    Updates

---
+++ precache-manifest.7ac466b1ca237875afa93dab2a57b859.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "620c32ba0c314fdea13b",
+    "url": "/portfolio/static/css/main.767099f4.chunk.css"
+  },
+  {
+    "revision": "620c32ba0c314fdea13b",
+    "url": "/portfolio/static/js/main.620c32ba.chunk.js"
+  },
+  {
+    "revision": "3d0490cabbdd50b1c19f",
+    "url": "/portfolio/static/js/1.3d0490ca.chunk.js"
+  },
+  {
+    "revision": "7ee9e0a1fd58eaae7c96",
+    "url": "/portfolio/static/js/2.7ee9e0a1.chunk.js"
+  },
+  {
+    "revision": "63d2feedb78bc5778401",
+    "url": "/portfolio/static/js/3.63d2feed.chunk.js"
+  },
+  {
+    "revision": "2f253b7783033fbcca3d",
+    "url": "/portfolio/static/js/runtime~main.2f253b77.js"
+  },
+  {
+    "revision": "14d9a85a878fa2540977cf8a223dc363",
+    "url": "/portfolio/static/media/logo.14d9a85a.png"
+  },
+  {
+    "revision": "27406c5da0f1d69b7c58bc87861b705d",
+    "url": "/portfolio/static/media/back1.27406c5d.png"
+  },
+  {
+    "revision": "6f800a1b9e75606bec77a08ed7fb5e32",
+    "url": "/portfolio/static/media/picture.6f800a1b.png"
+  },
+  {
+    "revision": "3d6b968bd5b7856010fec4cb8a721aa0",
+    "url": "/portfolio/static/media/my-foto.3d6b968b.jpg"
+  },
+  {
+    "revision": "5f02ebd98e7fe5a604c8a1567f438c88",
+    "url": "/portfolio/index.html"
+  }
+];
