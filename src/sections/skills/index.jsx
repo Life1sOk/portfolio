05bfa1705a81928cb6892ffb545f6f19f338a32c
Life1sOk <mr.lifeisok@gmail.com>
2023-05-09 01:21:25 +0600

    check

--- src/sections/skills/index.jsx
+++ src/sections/skills/index.jsx
@@ -6,8 +6,6 @@ import Title from "../../components/title/title.component";
 import SkillSetContainer from "../../containers/skill-set/skill-set.container";
 import BoxBig from "../../containers/box-big/box-big.component";
 
-import AnimationView from "../../routers/layouts/animation/animation-wrapper";
-
 import { MainWrapper, MainSkills, TitleWrapper, BoxWrapper } from "./index.style";
 
 const Skills = memo(
@@ -16,21 +14,17 @@ const Skills = memo(
 
     return (
       <MainWrapper ref={ref}>
-        <AnimationView>
-          <BoxWrapper>
-            <BoxBig />
-          </BoxWrapper>
-        </AnimationView>
-        <AnimationView>
-          <MainSkills>
-            <TitleWrapper>
-              <Title number={2} isReverse>
-                {t("tools.title")}
-              </Title>
-            </TitleWrapper>
-            <SkillSetContainer />
-          </MainSkills>
-        </AnimationView>
+        <BoxWrapper>
+          <BoxBig />
+        </BoxWrapper>
+        <MainSkills>
+          <TitleWrapper>
+            <Title number={2} isReverse>
+              {t("tools.title")}
+            </Title>
+          </TitleWrapper>
+          <SkillSetContainer />
+        </MainSkills>
       </MainWrapper>
     );
   })
