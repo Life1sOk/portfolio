b1748b5febd3254519af89b5626a0e77f668de71
Life1sOk <mr.lifeisok@gmail.com>
2023-05-09 16:05:09 +0600

    Updates

---
+++ precache-manifest.d8c42b4b861c2adbb157a74735614c2a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e26babb3d5551cd43d",
+    "url": "/portfolio/static/css/main.3bc50952.chunk.css"
+  },
+  {
+    "revision": "32e26babb3d5551cd43d",
+    "url": "/portfolio/static/js/main.32e26bab.chunk.js"
+  },
+  {
+    "revision": "e16a6bb05c5f96df20cb",
+    "url": "/portfolio/static/js/1.e16a6bb0.chunk.js"
+  },
+  {
+    "revision": "f3fed3d044a7b02c37bd",
+    "url": "/portfolio/static/js/2.f3fed3d0.chunk.js"
+  },
+  {
+    "revision": "7a6df87a0320214b9959",
+    "url": "/portfolio/static/js/3.7a6df87a.chunk.js"
+  },
+  {
+    "revision": "5d4ea4293d8bbd4c2675",
+    "url": "/portfolio/static/js/runtime~main.5d4ea429.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/portfolio/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "6344a616328e36ac811b8d7d50056cf8",
+    "url": "/portfolio/static/media/andrey-ru.6344a616.pdf"
+  },
+  {
+    "revision": "6db8bd4d9776ab3805ef21a279b1a038",
+    "url": "/portfolio/static/media/andrey-eng.6db8bd4d.pdf"
+  },
+  {
+    "revision": "117a9d89578cfe238d5b2110a09fd6f0",
+    "url": "/portfolio/static/media/back1.117a9d89.png"
+  },
+  {
+    "revision": "e7a5521618f2016609a499a1da503969",
+    "url": "/portfolio/static/media/picture.e7a55216.png"
+  },
+  {
+    "revision": "a5d48880d807afcfb40028265390cb33",
+    "url": "/portfolio/static/media/profile.a5d48880.png"
+  },
+  {
+    "revision": "6fb35c0d3eceec55a7d6d1babc693a29",
+    "url": "/portfolio/static/media/my-foto.6fb35c0d.jpg"
+  },
+  {
+    "revision": "cfa1f7ff4c1784d2b91e99c97c543766",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3fa41f9eef83ef6a87668ac97cd6572d.js"
+  "/portfolio/precache-manifest.d8c42b4b861c2adbb157a74735614c2a.js"
 );
 
 workbox.clientsClaim();
